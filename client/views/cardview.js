f7121240835834800b509c092a2aad1d039e6bd1
Paul Boon <gvr37.p.l.boon@gmail.com>
2021-06-26 16:53:40 +0200

    p

--- client/views/cardview.js
+++ client/views/cardview.js
@@ -2,7 +2,7 @@ function Cardview({imagesrc, onClick, children, style}){
     var defstyle = Object.assign({maxWidth:'200px', borderRadius:'3px', margin:'0px 10px 10px 0px'},style)
     return <div onClick={onClick} style={defstyle}>
         <img src={imagesrc} style={{width:'100%'}} />
-        <div className="card-body">
+        <div style={{padding:'5px'}}>
             {children}
         </div>
     </div>
